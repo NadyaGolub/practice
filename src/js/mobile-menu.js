05ee29d4e5bf0cb823dace156c67435f9eae25c1
NadyaGolub <nadyagolub@ukr.net>
2022-10-19 17:15:15 +0300

    Оновлення

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,40 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  // no-scroll
+  const bodyScrollLock = document.querySelector("body");
+
+  const mobileMenuList = document.querySelector('.js-menu-nav');
+
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    // no-scroll
+    bodyScrollLock.classList.toggle('scroll-no-menu');
+    
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+  mobileMenuList.addEventListener('click', toggleMenu);
+  
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+    // no-scroll
+     bodyScrollLock.classList.remove('scroll-no-menu');
+  });
+})();
